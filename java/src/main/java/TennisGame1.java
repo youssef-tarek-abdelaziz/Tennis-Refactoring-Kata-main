9d3f6e578de03310009b85f862f23608f65eb10a
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2025-12-22 19:42:27 +0200

    refactor advantage Winner logic

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -79,10 +79,10 @@ public class TennisGame1 implements TennisGame {
     private String getTheWinnerPlayer() {
         String score;
         int minusResult = m_score1 - m_score2;
-        if (minusResult==1) score ="Advantage " + player1;
-        else if (minusResult ==-1) score ="Advantage " + player2;
-        else if (minusResult>=2) score = "Win for " + player1;
-        else score ="Win for " + player2;
+        if (Math.abs(minusResult) == 1)
+            score ="Advantage " + (m_score1 > m_score2 ? player1 : player2);
+        else
+            score = "Win for " + (m_score1 > m_score2 ? player1 : player2);
         return score;
     }
 }

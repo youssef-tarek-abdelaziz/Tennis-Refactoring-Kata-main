3b2b391a77721726f3cc728aa598d21ab55e3010
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2025-12-22 20:01:55 +0200

    re-style some lines make player names variables final strings

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,8 +3,8 @@ public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -19,16 +19,14 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        if (m_score1==m_score2) {
+        String score;
+        if (m_score1 == m_score2) {
             score = getEqualityScore();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
+        else if (m_score1>=4 || m_score2>=4) {
             score = getLeadingPlayerResult();
         }
-        else
-        {
+        else {
             score = getCurrentResult();
         }
         return score;

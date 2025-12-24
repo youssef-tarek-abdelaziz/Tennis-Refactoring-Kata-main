3deba03db87b7e736ae49ac91fcd27bfe86cf779
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2025-12-24 18:22:49 +0200

    TennisGame2 - remove unused variables

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -4,8 +4,6 @@ public class TennisGame2 implements TennisGame
     public int P1point = 0;
     public int P2point = 0;
     
-    public String P1res = "";
-    public String P2res = "";
     private String player1Name;
     private String player2Name;
 

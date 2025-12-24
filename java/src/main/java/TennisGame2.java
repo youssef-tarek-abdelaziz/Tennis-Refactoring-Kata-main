3e1982e5fabff6c40004504570c2ee8de190e4fe
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2025-12-24 18:25:07 +0200

    TennisGame2 - introduce Scores Map

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TennisGame2 implements TennisGame
 {
@@ -6,10 +8,12 @@ public class TennisGame2 implements TennisGame
     
     private String player1Name;
     private String player2Name;
+    Map<Integer, String> scores;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        initScoresMap();
     }
 
     public String getScore(){
@@ -60,14 +64,17 @@ public class TennisGame2 implements TennisGame
             IncP2Score();
     }
 
+
+    private void initScoresMap() {
+        scores = new HashMap<>();
+        scores.put(0, "Love");
+        scores.put(1, "Fifteen");
+        scores.put(2, "Thirty");
+        scores.put(3, "Forty");
+    }
+
     private String getPlayerScore(int score) {
-        return switch (score) {
-            case 0 -> "Love";
-            case 1 -> "Fifteen";
-            case 2 -> "Thirty";
-            case 3 -> "Forty";
-            default -> "";
-        };
+        return scores.get(score);
     }
     private void IncP1Score(){
         P1point++;

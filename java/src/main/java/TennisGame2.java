c07df3b6fdc7a56a703e3b646054e5c63f7ff466
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2025-12-24 18:22:23 +0200

    TennisGame2 - move get current score logic to else statement

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -26,55 +26,8 @@ public class TennisGame2 implements TennisGame
                 score = getPlayerScore(P1point) + "-All";
             }
         }
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+        else {
+            score = getPlayerScore(P1point) + "-" + getPlayerScore(P2point);
         }
         
         if (P1point > P2point && P2point >= 3)
